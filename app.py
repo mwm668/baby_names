72df7fdfcbd6a12822750392c6c6fd79f3942973
Mitch McIntyre <mitchmcintyre668@gmail.com>
2021-08-30 21:58:30 +1200

    Added year or name functionality

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+
+######### Define function ###########
+# Move these to another file eventually
+def display_results(baby_name):
+    
+    # What happens if we don't find that name?
+    
+    # Get df filtered on that name
+    name_df = df[df.name == baby_name]
+    # Convert year to datetime, set as index
+    name_df['year'] = pd.to_datetime(name_df['year'], format="%Y")
+    name_df.set_index('year',inplace=True)
+
+    # Plot chart
+    st.subheader("Number of {0}'s born by year".format(baby_name))
+    st.bar_chart(name_df[['count']])
+
+    # Plot chart
+    # st.subheader("Rank of {0} by year".format(baby_name))
+    # st.line_chart(name_df[['rank']])
 
 # Set title
-st.title('NZ Baby Names')
+st.title('Baby Names of NZ')
+st.text('Select to search by name or year:')
 
 # Define dataframe
 df = pd.read_csv('nz_baby_names.csv').sort_values(by=['year','rank'])
@@ -13,52 +35,64 @@ df = pd.read_csv('nz_baby_names.csv').sort_values(by=['year','rank'])
 ####################
 
 # Define columns for widget layout
-left_column, right_column = st.columns(2)
+# try except because different versions of streamlit being annoying
+try:
+    left_column, right_column = st.columns(2)
+except:
+    left_column, right_column = st.beta_columns(2)
+
+# Select top-level of analysis
+year_or_name_select = left_column.radio('Search by',['Year','Name'])
+year_or_name_select = year_or_name_select.lower()
 
 # Select gender of name
-gender_select = left_column.radio('Gender',['Female','Male'])
+gender_select = right_column.radio('Gender',['Female','Male'])
 gender_select = gender_select.lower()
 
-# Select year
-year_select = right_column.slider(
+##### CONTROL FLOW ########
+if year_or_name_select == 'year':
+    #########################
+    # Specfic YEAR analysis #
+    #########################
+
+    # Select year
+    year_select = st.slider(
     label="Year",
     min_value=1954,
     max_value=2020
     )
 
-# Write top names for year_select
-top_df = df[(df.year == year_select) & (df.gender == gender_select) &  (df['rank'] <= 10)][['rank','name','count']]
-top_df.set_index('rank',inplace=True)
-top_df['rank'] = top_df.index
+    # Write top names for year_select
+    top_df = df[(df.year == year_select) & (df.gender == gender_select) &  (df['rank'] <= 10)][['rank','name','count']]
+    top_df.set_index('rank',inplace=True)
+    top_df['rank'] = top_df.index
 
-if st.checkbox('Show top names'):
-    st.write("{0} names for {1}".format(gender_select.capitalize(),year_select))
-    st.write(top_df)
+    if st.checkbox('Show top names'):
+        left_column.write("{0} names for {1}".format(gender_select.capitalize(),year_select))
+        left_column.write(top_df)
+        baby_name = right_column.selectbox(options=top_df['name'], label='Select other top name')
+        display_results(baby_name)
+    else:
+        # Use top name as default for specific name chart
+        baby_name = top_df.loc[1]['name']  
+        display_results(baby_name)
 
-# Use top name as default for specific name chart
-baby_name = top_df.loc[1]['name']
+elif year_or_name_select == 'name':
+    #########################
+    # Specfic NAME analysis #
+    #########################
 
-#########################
-# Specfic name analysis #
-#########################
+    # Set specific name
+    baby_name = 'John'
+    st.text_input("Enter baby name:", value=baby_name,key="name")
+    baby_name = st.session_state.name.capitalize()
 
-# Set specific name
-st.text_input("Enter baby name:", value=baby_name,key="name")
-baby_name = st.session_state.name.capitalize()
+    # display results
+    display_results(baby_name)
 
-# Get df filtered on that name
-name_df = df[df.name == baby_name]
-# Convert year to datetime, set as index
-name_df['year'] = pd.to_datetime(name_df['year'], format="%Y")
-name_df.set_index('year',inplace=True)
+else:
+    st.text('Choose already!')
 
-# Plot chart
-st.subheader("Number of {0}'s born by year".format(baby_name))
-st.bar_chart(name_df[['count']])
-
-# Plot chart
-st.subheader("Rank of {0} by year".format(baby_name))
-st.line_chart(name_df[['rank']])
 
 ##############
 # Disclaimer #
