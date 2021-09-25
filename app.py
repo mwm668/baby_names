21046b4251b38dc4bd8415ed2a97a171c78bba0d
Mitch McIntyre <mitchmcintyre668@gmail.com>
2021-09-25 17:37:13 +1200

    Added vertical year line, altair for name chart, decade metrics

--- app.py
+++ app.py
@@ -15,18 +15,27 @@ def display_results(df,baby_name):
     name_df = df[df.name == baby_name]
 
     # Convert year to datetime, set as index
-    name_df['year'] = pd.to_datetime(name_df['year'], format="%Y")
-    name_df.set_index('year',inplace=True)
+    # name_df['year'] = pd.to_datetime(name_df['year'], format="%Y")
+    # name_df.set_index('year',inplace=True)
 
     # Plot chart
     # st.subheader("Number of {0}'s born by year".format(baby_name))
     # st.bar_chart(name_df[['count']])
 
     # Plot chart
-    st.subheader("Number of {0}s born by year".format(baby_name))
-    st.line_chart(name_df[['count']])
+    # st.subheader("Number of {0}s born by year".format(baby_name))
+    # st.line_chart(name_df[['count']])
 
-    # st.balloons()
+    # Create altair chart object
+    st.subheader("Number of {0}s born by year".format(baby_name))
+    c = alt.Chart(name_df).mark_line(interpolate='basis',strokeWidth=3).encode(
+                alt.X('year(year):T', title='Year'),
+                alt.Y('count', title='Count')
+            )
+
+    # Display the chart           
+    st.altair_chart(c, use_container_width=True)
+
 
 def display_multiple_line_charts(df):
     """
@@ -40,9 +49,10 @@ def top_names_history(df,names_year,names_gender):
     Take dataframe, gender and year, return df of complete history of top X names
     """
     
-    # Get top names for year
+    # Get top names for given year
     top_names_list = get_top_names(df,names_year=names_year,names_gender=names_gender)['name'].unique()
 
+    # Return df of data for all those top names
     history_df = df[df['name'].isin(top_names_list)]
 
     return history_df
@@ -62,6 +72,20 @@ def get_top_names(df,names_year=2020,names_gender='female',top_x=5):
 
     return top_df
 
+# Given a year, convert to decade for data grouping
+def year_to_decade(year):
+    decade = int(str(year)[:3]+'0')
+    return decade
+
+# Given a name, return probability of decades
+def decade_probability(df,baby_name):
+    name_df = df[df['name']==baby_name]
+    name_df['decade'] = name_df.apply(lambda x: year_to_decade(x['year']),axis=1)
+    decade_df = name_df.groupby(by=name_df['decade'])['count'].sum().reset_index(name='count')
+    decade_df['prob'] = decade_df['count'] / decade_df['count'].sum()
+    decade_df.sort_values('prob',ascending=False,inplace=True)
+    return decade_df
+
 def display_metrics(df,baby_name):
     """
     Take df and name and generate 3 metric cards in Streamlit
@@ -74,21 +98,33 @@ def display_metrics(df,baby_name):
     highest_year = name_df2.iloc[0].year
     highest_year_count = name_df2.iloc[0]['count']
     total_count = name_df2['count'].sum()
+
+    # Get decade metric
+    decade_df = decade_probability(df,baby_name)
+    top_decade = int(decade_df.iloc[0].decade)
+    top_prob = decade_df.iloc[0].prob
     
     # Display metrics side-by-side
-    col1, col2, col3 = st.columns(3)
-    col1.metric("Most Popular Year",str(highest_year))
-    col2.metric("{0}s in {1}".format(baby_name,highest_year),human_format(highest_year_count))
-    col3.metric("Total {}s".format(baby_name),human_format(total_count))
+    st.subheader('Interesting facts about {}'.format(baby_name))
+    col1a, col2a, col3a = st.columns(3)
+    col1a.metric("Most Popular Year",str(highest_year))
+    col2a.metric("Total {}s".format(baby_name),human_format(total_count))
+    col3a.metric("Most popular decade",str(top_decade)+"s")
+    
+    
+    col1b, col2b, col3b = st.columns(3)
+    col1b.metric("{0}s in {1}".format(baby_name,highest_year),human_format(highest_year_count))
+    col3b.markdown("**{0:.0%}** of {1}s were born in the **{2}s**".format(top_prob,baby_name,top_decade))
 
 
 def human_format(num):
     magnitude = 0
-    while abs(num) >= 1000:
+    while abs(num) >= 10000:
         magnitude += 1
         num /= 1000.0
     # add more suffixes if you need them
-    return '%.1f%s' % (num, ['', 'k', 'M', 'G', 'T', 'P'][magnitude])
+    return '%.0f%s' % (num, ['', 'k', 'M', 'G', 'T', 'P'][magnitude])
+
 
 # Set title
 st.title('👶 Baby Names of NZ 👶')
@@ -159,11 +195,15 @@ if year_or_name_select == 'year':
 
     # Create size column to highlight top name for current year
     history_df['size'] = np.where(history_df['name']==baby_name, 2, 1)
+
+    # Get min/max for axis
+    min_year = history_df['year'].min()
+    max_year = history_df['year'].max()
     
     # Create altair chart object
     selection = alt.selection_multi(fields=['name'], bind='legend')
     c = alt.Chart(history_df).mark_line(interpolate='basis').encode(
-                alt.X('year(year):T', title='Year'),
+                alt.X('year(year):T', title='Year', scale=alt.Scale(domain=(min_year-5, max_year))),
                 alt.Y('count', title='Count'),
                 alt.Color('name', title='Name', scale=alt.Scale(scheme='category10')), 
                 alt.Size('size'),
@@ -171,9 +211,21 @@ if year_or_name_select == 'year':
                 opacity=alt.condition(selection, alt.value(1), alt.value(0.2))
             ).add_selection(selection)
 
-    # Display the chart           
-    st.altair_chart(c, use_container_width=True)
+    # Add vertical line for selected year
+    rule = alt.Chart(pd.DataFrame({
+                    'year': [year_select],
+                    'color': ['steelblue']
+                })).mark_rule().encode(
+                    x='year:T',
+                    color=alt.Color('color:N', scale=None)
+                )
 
+    year_text = rule.mark_text(align='center', xOffset = 15, yOffset = 110)\
+                .encode(text='year(year):T')
+
+
+    # Display the chart           
+    st.altair_chart(c + rule + year_text, use_container_width=True)
 
 elif year_or_name_select == 'name':
     #########################
